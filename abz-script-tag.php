9d256a87927786f983a1b2e82483f44eb1f6f242
Yassine <yassine.abouzia@gmail.com>
2023-12-28 14:40:20 +0100

    include settings api file

--- abz-script-tag.php
+++ abz-script-tag.php
@@ -25,6 +25,7 @@ require_once(ABZ_SCRIPT_TAG_PLUGIN_PATH . 'includes/setup-options.php');
 require_once(ABZ_SCRIPT_TAG_PLUGIN_PATH . 'includes/display-script.php');
 require_once(ABZ_SCRIPT_TAG_PLUGIN_PATH . 'includes/admin-menus.php');
 require_once(ABZ_SCRIPT_TAG_PLUGIN_PATH . 'includes/enqueue-scripts.php');
+require_once(ABZ_SCRIPT_TAG_PLUGIN_PATH . 'includes/settings-api.php');
 
 /** Upon deactivation */
 register_deactivation_hook(__FILE__, 'abz_script_tag_deactivation');
